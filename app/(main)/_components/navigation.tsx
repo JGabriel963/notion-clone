4dbf9cf3831b1c0e6f450f5dc9a1e4fb19ab0511
João Gabriel <joaogabriel9633@gmail.com>
2025-11-02 21:19:51 -0300

    trash box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,9 +3,11 @@ import { cn } from "@/lib/utils";
 import {
   ChevronLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ComponentRef, useEffect, useRef, useState } from "react";
@@ -16,6 +18,12 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 export default function Navigation() {
   const pathname = usePathname();
@@ -141,6 +149,18 @@ export default function Navigation() {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? "bottom" : "right"}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
--- convex/documents.ts
+++ convex/documents.ts
@@ -95,3 +95,94 @@ export const create = mutation({
     return documents;
   },
 });
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identify = await ctx.auth.getUserIdentity();
+
+    if (!identify) throw new Error("Not authenticated");
+
+    const userId = identify.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), true))
+      .order("desc")
+      .collect();
+
+    return documents;
+  },
+});
+
+export const restore = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identify = await ctx.auth.getUserIdentity();
+
+    if (!identify) throw new Error("Not authenticated");
+
+    const userId = identify.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) throw new Error("Not found");
+
+    if (existingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const recursiveRestore = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: false,
+        });
+
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const option: Partial<Doc<"documents">> = {
+      isArchived: false,
+    };
+
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+      if (parent?.isArchived) {
+        option.parentDocument = undefined;
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, option);
+
+    recursiveRestore(args.id);
+
+    return document;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identify = await ctx.auth.getUserIdentity();
+
+    if (!identify) throw new Error("Not authenticated");
+
+    const userId = identify.subject;
+
+    const existiingDocument = await ctx.db.get(args.id);
+
+    if (!existiingDocument) throw new Error("Not found");
+
+    if (existiingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const document = await ctx.db.delete(args.id);
+
+    return document;
+  },
+});
