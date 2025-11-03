5ef4d447d19cd891a1871eab862f075b6acdb494
João Gabriel <joaogabriel9633@gmail.com>
2025-11-03 13:40:38 -0300

    Search functionality

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import React from "react";
 import Navigation from "./_components/navigation";
+import { SearchCommand } from "@/components/search-command";
 
 export default function MainLayout({
   children,
@@ -28,7 +29,10 @@ export default function MainLayout({
   return (
     <div className="h-full flex dark:bg-[#1f1f1f]">
       <Navigation />
-      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
     </div>
   );
 }
